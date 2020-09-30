35f2d2b5af35f21f02032fb3273ae84f46dcda08
jiro.suzuki <cloud9.weekly@gmail.com>
2020-09-30 12:34:35 +0000

    post

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -8,16 +8,26 @@
                     <h3 class="card-title">{{ $user->name }}</h3>
                 </div>
                 <div class="card-body">
-                    <img class="rounded img-fluid" src="{{ Gravatar::get($user->email, ['size' => 500]) }}" alt="">
+                    <img class="rounded img-fluid" src="{{ Gravatar::get($user->email, ['size' => 500 ]) }}" alt="">
                 </div>
             </div>
         </aside>
         <div class="col-sm-8">
             <ul class="nav nav-tabs nav-justified mb-3">
-                <li class="nav-item"><a href="#" class="nav-link">TimeLine</a></li>
+                <li class="nav-item">
+                    <a href="{{ route('users.show', ['user' => $user->id]) }}" class="nav-link {{ Request::routeIs('users.show') ? 'active' : '' }}">
+                        TimeLine
+                        <span class="badge badge-secondary">{{ $user->microposts_count }}</span>
+                    </a>
+                </li>
                 <li class="nav-item"><a href="#" class="nav-link">Followings</a></li>
                 <li class="nav-item"><a href="#" class="nav-link">Followers</a></li>
             </ul>
+            @if (Auth::id() == $user->id)
+                @include('microposts.form')
+            @endif
+            
+            @include('microposts.microposts')
         </div>
     </div>
 @endsection
